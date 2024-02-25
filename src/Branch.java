be66f3b9440333b21804bb74ee41c4379fa5d444
leo <horvath.leo2001@gmail.com>
2024-02-26 00:39:26 +0100

    Always store parent Branch for classical tree structure
    Lines are no longer drawn when creating Branch object

--- src/Branch.java
+++ src/Branch.java
@@ -1,16 +1,16 @@
-import processing.core.PApplet;
 import processing.core.PGraphics;
 
 public class Branch {
+    public Branch parent;
     public double[] start;
     public double[] end;
     public double angle;
 
-    public Branch(PGraphics graphicsHandler, double x1, double y1, double x2, double y2, double theta){
+    public Branch(Branch parent, PGraphics graphicsHandler, double x1, double y1, double x2, double y2, double theta){
+        this.parent = parent;
         this.start = new double[]{x1,y1};
         this.end = new double[]{x2, y2};
         this.angle = theta;
 
-        graphicsHandler.line((float)x1, (float)y1, (float)x2, (float)y2);
     }
 }
